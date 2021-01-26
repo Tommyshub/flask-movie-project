b909b89ef4ab30eb8ffdcd39c34280c061136274
Gretaah <fluganisoppan@gmailcom>
2021-01-26 14:50:12 +0100

    Replaced imdb with tmdb blueprint (the movie database) and added blueprint for error handling

--- tmdb.py
+++ tmdb.py
@@ -2,4 +2,6 @@ from flask import Blueprint, render_template
 from tmdb3 import searchMovie
 
 
-tmdb = Blueprint("tmdb", __name__, static_folder="static", template_folder="templates"
+tmdb = Blueprint("tmdb", __name__, static_folder="static", template_folder="templates")
+
+
