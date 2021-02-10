660a2268d2e126e5b9b38486167e2fbdf2185ce6
Gretaah <fluganisoppan@gmailcom>
2021-02-10 19:31:33 +0100

    connect review form to database

--- error_handlers.py
+++ error_handlers.py
@@ -15,3 +15,14 @@ error_messages = {
     504: ('Gateway Timeout!', 'The connection to the server was lost.')
 }
 
+
+@error_handlers.app_errorhandler(HTTPException)
+def handle_bad_request(e):
+    # Render error.html if there's an attribute error
+    if isinstance(e, AttributeError):
+        return render_template("error.html")
+    # Print the error message if the error code exists in error messages
+    if e.code in error_messages:
+        error_message = error_messages[e.code]
+        return render_template(f"{e.code}.html", error_message=error_message)
+    return render_template("error.html")
--- movie_search.py
+++ movie_search.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, Blueprint, render_template, request, redirect, url_for, Response, abort
+from flask import Flask, flash, Blueprint, render_template, request, redirect, url_for, Response, session, abort
 import requests
 from tmdbv3api import TMDb, Movie
 from forms import SearchForm, MovieForm
@@ -20,10 +20,9 @@ tmdb.api_key = os.environ.get("TMDB_KEY")
 tmdb.language = "en"
 
 @movie_search.route("/home", methods=["POST", "GET"])
-
 def home():
     # Fetch all information about all movies from the movies database in order to display it in the html
-    moviez = mongo.db.movies.find({}, {'movie_id': 1, 'movie_title': 1, 'movie_overview': 1, 'poster_path': 1, '_id': 0})
+    home_movies = mongo.db.movies.find({}, {'movie_id': 1, 'movie_title': 1, 'movie_overview': 1, 'poster_path': 1, '_id': 0})
     form = SearchForm()
     if form.validate_on_submit():
         # Setting user input to string
@@ -32,31 +31,38 @@ def home():
         flash(f"Searching for {string}...")
         # Search for movie with user input from html form
         movies = movie.search(string)
-        return render_template('home.html', movies=movies, moviez=list(moviez), form=form)
-    return render_template('home.html', moviez=list(moviez), form=form)
+        return render_template('home.html', movies=movies, home_movies=list(home_movies), form=form)
+    return render_template('home.html', home_movies=list(home_movies), form=form)
 
 
 
 @movie_search.route("/review/<movie_id>", methods=["POST", "GET"])
 def review(movie_id):
     form = MovieForm()
+    # Get currrent user 
+    user = mongo.db.users.find_one(
+    {"username": session["user"]})["username"].capitalize()
     # Create veriables for title, overview and post path. 
     movie = mongo.db.movies.find_one({"movie_id": movie_id})
     movie_title = movie['movie_title']
     movie_overview = movie['movie_overview']
     poster_path = movie['poster_path']
-     # Check if the movie id already exists in db
-    existing_movie = mongo.db.movies.find_one(
-        {"movie_id": request.form.get("movie_id")})
+    # Information about the review to send to the database 
+    review_info = {
+        "movie_id": movie_id,
+        "username": user,
+        "review_text": request.form.get("review")
+    }
     # If the user press create and if the movie does not exist in the database 
-    if request.method == 'POST' and not existing_movie:
+    if request.method == 'POST' and not movie:
         # Insert movie id, title poster path and overview in the database
         mongo.db.movies.insert_one({'movie_id': request.form['movie_id'], 
                                     'movie_title': request.form['movie_title'],
                                     'poster_path': request.form['poster_path'],
                                     'movie_overview': request.form['movie_overview']})
-    # If the user press create and if the movie exists in the database
-    elif request.method == 'POST' and existing_movie:
-        print("Movie exists already")
+    # Send information from the review form to the database 
+    if request.method == 'POST':
+        mongo.db.reviews.insert_one(review_info)
+        print(review_info)
     return render_template('review.html', form=form, 
     movie_id=movie_id, movie_title=movie_title, movie_overview=movie_overview, poster_path=poster_path)
