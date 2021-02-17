e67efed241a4f8bba03ae2470edc5f11912c28d4
Gretaah <fluganisoppan@gmailcom>
2021-02-17 21:02:36 +0100

    change tmdb api wrapper

--- movie_search.py
+++ movie_search.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, Blueprint, render_template, request, redirect, url_for, Response, session, abort
 import requests
-from tmdbv3api import TMDb, Movie
+import tmdbsimple as tmdb, requests
 from forms import SearchForm, MovieForm
 from bson.objectid import ObjectId
 from database import mongo
@@ -10,15 +10,14 @@ if os.path.exists("env.py"):
 
 # Blueprint for this file
 movie_search = Blueprint("movie_search", __name__, static_folder="static", template_folder="templates")
-# Create an instance of TMDb
-tmdb = TMDb()
-# Create an instance of the movie functionn
-movie = Movie()
 # Get api key from env.py
-tmdb.api_key = os.environ.get("TMDB_KEY")
+tmdb.API_KEY = os.environ.get("TMDB_KEY")
 # Default language for imports 
 tmdb.language = "en"
+# Request session
+tmdb.REQUESTS_SESSION = requests.Session()
 
+search = tmdb.Search()
 
 @movie_search.route("/home", methods=["POST", "GET"])
 def home():
@@ -31,9 +30,14 @@ def home():
         # Display what user is searching for
         flash(f"Searching for {string}...")
         # Search for movie with user input from html form
-        movies = movie.search(string)
-        return render_template('home.html', movies=movies, home_movies=list(home_movies), form=form)
-    return render_template('home.html', home_movies=list(home_movies), form=form)
+        movies = search.movie(query=string)
+        for res in search.results:
+            print(res['title']) 
+            print(res['id']) 
+            print(res['overview']) 
+            print(res['poster_path'])
+        return render_template('home.html', movies=movies, search=search, home_movies=list(home_movies), form=form)
+    return render_template('home.html', search=search, home_movies=list(home_movies), form=form)
 
 
 @movie_search.route("/review/<movie_id>", methods=["POST", "GET"])
@@ -43,12 +47,13 @@ def review(movie_id):
     user = mongo.db.users.find_one(
     {"username": session["user"]})["username"].capitalize()
     # Create veriables for title, overview and post path.
-    movie = mongo.db.movies.find_one({"movie_id": movie_id})
-    # Information about the review to send to the database
-    for key, value in movie.items():
-        movie_title = movie["movie_title"]
-        movie_overview = movie["movie_overview"]
-        poster_path = movie["poster_path"]
+    movie = tmdb.Movies(movie_id)
+    response = movie.info()
+    
+    movie_title = movie.title
+    movie_id = movie.id
+    movie_overview = movie.overview
+    poster_path = movie.poster_path
     # Fetch all information about all movies from the movies database in order to display it in the html
     reviews = mongo.db.reviews.find({}, {'movie_id': 1, 'movie_title': 1, 'username': 1, 'review_text': 1, '_id': 1})
     # If the user press create and if the movie does not exist in the database 
@@ -56,9 +61,9 @@ def review(movie_id):
         # Information about the movie to send to the database
         movie_info = {
             "movie_id": movie_id,
-            "movie_title": request.form.get("movie_title"),
-            "movie_overview": request.form.get("movie_overview"),
-            "poster_path": request.form.get("poster_path")
+            "movie_title": movie_title,
+            "movie_overview": movie_overview,
+            "poster_path": poster_path
         }
         # Insert movie informationin the database
         mongo.db.movies.insert_one(movie_info)
@@ -76,4 +81,4 @@ def review(movie_id):
     if form.delete.data and request.method == 'POST':
             delete_id = request.form.get("delete")
             mongo.db.reviews.remove( {"_id": ObjectId(delete_id) });
-    return render_template('review.html', form=form, reviews=list(reviews), user=user, movie_id=movie_id, movie_title=movie_title, movie_overview=movie_overview, poster_path=poster_path)
+    return render_template('review.html', form=form, reviews=list(reviews), movie_id=movie_id, movie_title=movie_title, movie_overview=movie_overview, poster_path=poster_path, user=user)
