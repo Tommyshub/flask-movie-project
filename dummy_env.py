e937a9ce07fb82dcd99baa11dd10b40533e4cd73
Tommy B <tommy@whatever>
2021-06-24 15:29:15 +0200

    add dummy env file for local deployment

---
+++ dummy_env.py
@@ -0,0 +1,17 @@
+"""
+In order to be able to run this app you need to rename this file to env.py,
+uncomment the code below and add your secret key,
+mongo uri, mongo database name and the uri for your redis server.
+"""
+
+""" import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "")
+os.environ.setdefault(
+    "MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "")
+os.environ.setdefault("TMDB_KEY", "")
+os.environ.setdefault(
+    "REDIS_URI", "") """
